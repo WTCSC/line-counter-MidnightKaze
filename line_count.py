0a563a2282174eb72d96fe91e7e12a1db6d9527c
Zachary Flower <zach@flower.codes>
2024-09-03 17:23:37 -0600

    Update line_count.py
    @MidnightKaze fyi, your submission got messed up in GitHub, so I'm pulling it over and fixing it here

--- line_count.py
+++ line_count.py
@@ -1,4 +1,9 @@
 def line_count():
-  """
-  Reference the "Reading Files in Python" lesson to open the `file.txt` file, count the number of words in the file, and return the count.
-  """
+  
+  file = open('file.txt', 'r') #opens the file (file.txt) in read mode
+  
+  number_lines = len(file.readlines()) #states that the number of lines is equal to the length of the resulting list from running the readlines function
+  
+  file.close() #closes the file
+  
+  return('number_lines') #returns the number of lines from above
